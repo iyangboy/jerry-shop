0882242c7e641d413fd5a068d41ff3d21d236d2c
杨万里 <1091231787@qq.com>
2019-11-12 10:43:24 +0800

    修改和删除收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -22,6 +22,7 @@ class UserAddressesController extends Controller
         return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
     }
 
+    // 修改提交
     public function store(UserAddressRequest $request)
     {
         $request->user()->addresses()->create($request->only([
@@ -35,5 +36,42 @@ class UserAddressesController extends Controller
         ]));
 
         return redirect()->route('user_addresses.index');
+    }
+
+    // 编辑页
+    public function edit(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+
+    // 更新操作
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $this->authorize('own', $user_address);
+
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+    // 删除操作
+    public function destroy(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+
+        $user_address->delete();
+
+        return [];
+        //return redirect()->route('user_addresses.index');
     }
 }
