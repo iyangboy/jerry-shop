f1bce866e0ad83fff6b5c4959878433058f266c5
杨万里 <1091231787@qq.com>
2019-12-24 11:37:48 +0800

    集成微信支付

--- config/pay.php
+++ config/pay.php
@@ -11,11 +11,11 @@ return [
     ],
 
     'wechat' => [
-        'app_id'      => '',
-        'mch_id'      => '',
-        'key'         => '',
-        'cert_client' => '',
-        'cert_key'    => '',
+        'app_id'      => 'wxd85b6c50ba814b52',
+        'mch_id'      => '1564157731',
+        'key'         => 'DatojfIRsZVjTaFgcMd0rD4oonN3IYkF',
+        'cert_client' => resource_path('wechat_pay/apiclient_cert.pem'),
+        'cert_key'    => resource_path('wechat_pay/apiclient_key.pem'),
         'log'         => [
             'file' => storage_path('logs/wechat_pay.log'),
         ],
