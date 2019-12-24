de85803116b57111768c85a696159806625f46bc
杨万里 <1091231787@qq.com>
2019-12-24 18:29:49 +0800

    管理后台 - 优惠券列表

--- app/Models/CouponCode.php
+++ app/Models/CouponCode.php
@@ -54,13 +54,16 @@ class CouponCode extends Model
         $str = '';
 
         if ($this->min_amount > 0) {
-            $str = '满'.str_replace('.00', '', $this->min_amount);
+            // $str = '满'.$this->min_amount;
+            $str = '满' . str_replace('.00', '', $this->min_amount);
         }
         if ($this->type === self::TYPE_PERCENT) {
-            return $str.'优惠'.str_replace('.00', '', $this->value).'%';
+            // return $str.'优惠'.$this->value.'%';
+            return $str . '优惠'.str_replace('.00', '', $this->value).'%';
         }
 
-        return $str.'减'.str_replace('.00', '', $this->value);
+        // return $str.'减'.$this->value;
+        return $str . '减' . str_replace('.00', '', $this->value);
     }
 
 }
